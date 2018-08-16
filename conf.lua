7fcfe40323a392f902d62e9adcefb568b6a23d32
Crazy-Pie <need844@gmail.com>
2018-08-16 20:11:59 +0500

    New file in this project.

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+t.window.width = 427
+t.window.height = 240      
+t.window.msaa = false
+t.window.fsaa = false
+t.window.vsync = false
+t.window.fullscreen = true
+t.window.fullscreentype = "exclusive" 
+
+end
